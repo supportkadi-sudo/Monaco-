5b9dd944d389d6fb6a149c110c52c1d7aecee286
supportkadi-sudo <supportkadi@gmail.com>
2026-08-27 05:13:13 +0500

    Keep desktop header navigation concise

--- components/site/Header.tsx
+++ components/site/Header.tsx
@@ -13,7 +13,6 @@ export function Header() {
         <a href="#zones">Зоны</a>
         <a href="#prices">Цены</a>
         <a href="#gallery">Галерея</a>
-        <a href="#before-visit">Перед посещением</a>
         <a href="#contacts">Контакты</a>
       </nav>
       <div className="header-actions">
